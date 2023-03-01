56c9c763e5a44709b19cd6f4cbe348fef45e12c9
Nicholas Peters <peters.nicholas@gmail.com>
2023-03-01 15:00:10 +0100

    connect front end to back end

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Front end JS file is loaded!");
+fetch("http://localhost:3000/users")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    document.querySelector("#userList").innerHTML = data
+      .map((user) => {
+        return `<li>${user.name}</li>`;
+      })
+      .join("");
+  });
