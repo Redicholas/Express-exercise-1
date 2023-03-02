b2f74e3f2756a463cb8b85bf42cef9868143037c
Nicholas Peters <peters.nicholas@gmail.com>
2023-03-02 10:02:06 +0100

    add pw input to new user, add login form

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const userNameInput = document.querySelector("#userNameInput");
+const userPasswordInput = document.querySelector("#userPasswordInput");
 const addUserBtn = document.querySelector("#submitUserBtn");
+
+const loginNameInput = document.querySelector("#loginNameInput");
+const loginPasswordInput = document.querySelector("#loginPasswordInput");
+const loginBtn = document.querySelector("#loginBtn");
+
 const userList = document.querySelector("#userList");
 
 fetch("http://localhost:3000/users")
@@ -12,34 +18,17 @@ function renderUsers(users) {
   userList.innerHTML = "";
 
   users.map((user) => {
-    let li = document.createElement("li");
-    li.id = user.id;
-    li.innerText = user.name;
+    const li = document.createElement("li");
+    li.innerHTML = `${user.id} : ${user.name}`;
     userList.appendChild(li);
   });
-  //   userList.innerHTML = "";
-
-  //   for (let i = 0; i < users.length; i++) {
-  //     const li = document.createElement("li");
-  //     li.innerHTML = `${users[i].id} : ${users[i].name}`;
-  //     userList.appendChild(li);
-
-  //   users.map((user) => {
-  //     const li = document.createElement("li");
-  //     li.innerHTML = `${user.id} : ${user.name}`;
-  //     userList.appendChild(li);
-  //   });
-
-  //   userList.innerHTML = users
-  //     .map((user) => {
-  //       console.log(user);
-  //       return `<li>${user.id} : ${user.name}</li>`;
-  //     })
-  //     .join("");
 }
 
 addUserBtn.addEventListener("click", () => {
-  let user = { name: userNameInput.value };
+  let user = { name: userNameInput.value, password: userPasswordInput.value };
+  console.log(user);
+  userNameInput.value = "";
+  userPasswordInput.value = "";
 
   fetch("http://localhost:3000/users", {
     method: "POST",
@@ -57,39 +46,3 @@ addUserBtn.addEventListener("click", () => {
         });
     });
 });
-
-// addUserBtn.addEventListener("click", () => {
-//   // SKAPA EN NY ANVÄNDARE
-//   let user = { name: userNameInput.value };
-//   console.log(user);
-
-//   // SKICKA TILL SERVERN
-//   fetch("http://localhost:3000/users", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(user),
-//   })
-//     .then((res) => res.json())
-//     .then((data) => {
-//       renderUsers(data);
-//     });
-// });
-
-// addUserBtn.addEventListener("click", () => {
-//   let user = { name: userNameInput.value };
-//   console.log("step 1: " + user);
-//   fetch("http://localhost:3000/users", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(user),
-//   })
-//     .then((response) => response.json())
-//     .then((data) => {
-//       renderUsers(data);
-//       console.log("step 2: " + data);
-//     });
-// });
